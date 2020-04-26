46fbba04e2a72ac46f335105ac359cf55a1e8a2c
Xin Zhang <xz5@princeton.edu>
2020-04-26 11:19:16 -0400

    added updated weak convergence outputs

--- weak.sh
+++ weak.sh
@@ -7,16 +7,16 @@
 #SBATCH -n 16 # number of cores
 
 #SBATCH --mem 1024 # memory to be used per node
-#SBATCH -t 16:00:00 # time (D-HH:MM)
+#SBATCH -t 48:00:00 # time (D-HH:MM)
 #SBATCH --mail-type=START,END,FAIL # notifications for job done & fail
 #SBATCH --mail-user=xzhang@pppl.gov # send-to address
 
 NPART=2048
-L=8
+L=7
 TRIALS=32
 
-python convTests.py -w EM $NPART $L 0.1 $TRIALS 0
-for cor in 0 1 2 5
+#python convTests.py -w EM $NPART $L 0.1 $TRIALS 0
+for cor in 5
 do
     python convTests.py -w MEM $NPART $L 0.1 $TRIALS $cor
 done
