bac8c9373bffe59d9349b51d8ed2d0431f27cb29
Xin Zhang <xz5@princeton.edu>
2020-05-04 09:17:12 -0400

    got rid of print statements in KL; added outputs for tTot=0.01s

--- convTests.py
+++ convTests.py
@@ -2,7 +2,7 @@ from Coulomb import *
 from plotting import *
 import sys, getopt
 
-OUTDIR = 'output/base2/small/'
+OUTDIR = 'output/base2/low/'
 
 def main(argv):    
     try:
--- weak.sh
+++ weak.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-#SBATCH --job-name=weakEM0
+#SBATCH --job-name=weak5
 
-
-#SBATCH -N 4 # number of nodes
-#SBATCH -n 16 # number of cores
+#SBATCH -n 64 # number of cores
 
 #SBATCH --mem 1024 # memory to be used per node
 #SBATCH -t 48:00:00 # time (D-HH:MM)
 #SBATCH --mail-type=START,END,FAIL # notifications for job done & fail
 #SBATCH --mail-user=xzhang@pppl.gov # send-to address
 
-NPART=1024
-L=9
+NPART=8192
+L=10
 TRIALS=32
+TTOT=0.01
 
-python convTests.py -w EM $NPART $L 0.1 $TRIALS 0
-for cor in 0
+#python convTests.py -w EM $NPART $L $TTOT $TRIALS 0
+for cor in 5
 do
-    python convTests.py -w MEM $NPART $L 0.1 $TRIALS $cor
+    python convTests.py -w MEM $NPART $L $TTOT $TRIALS $cor
 done
 
